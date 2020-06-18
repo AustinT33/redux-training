7d520cfaa74f55683202cae6d3d68a581c788f00
austint33 <austintumlinson@gmail.com>
2020-06-18 16:56:19 -0500

    created List component

---
+++ src/js/components/List.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const select = state => {
+    return { articles: state.articles };
+};
+
+const ConnectedList = ({ articles }) => (
+    <ul>
+        {articles.map(el => (
+            <li key={el.id}>{el.title}</li>
+        ))}
+    </ul>
+);
+
+const List = connect(select)(ConnectedList);
+
+export default List;
