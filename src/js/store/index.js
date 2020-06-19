4a39671518aba9c741957889fdf73a94ee284be4
austint33 <austintumlinson@gmail.com>
2020-06-19 18:48:24 -0500

    incorporated redux thunk into redux store

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { badWordsMiddlware } from "../middleware";
+import thunk from "redux-thunk";
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(badWordsMiddlware),
+    storeEnhancers(applyMiddleware(badWordsMiddlware, thunk))
     );
 
 export default store;
