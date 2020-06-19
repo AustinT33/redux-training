f9f26ab2150cd1adbd39159aac929f7baf22b8e6
austint33 <austintumlinson@gmail.com>
2020-06-19 18:27:15 -0500

    included applyMiddlware into redux store

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
+import { badWordsMiddlware } from "../middlware";
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    applyMiddleware(badWordsMiddlware),
+    );
 
 export default store;
