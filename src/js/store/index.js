4641f08f46cafa50a3f64fdb097882998102b950
austint33 <austintumlinson@gmail.com>
2020-06-20 13:47:25 -0500

    refactored store to use saga instead of thunk

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,13 +1,20 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { badWordsMiddlware } from "../middleware";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import apiSaga from "../sagas/api-saga";
+
+const initializeSagaMiddleware = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    storeEnhancers(applyMiddleware(badWordsMiddlware, thunk))
-    );
+    storeEnhancers(
+        applyMiddleware(badWordsMiddlware, initializeSagaMiddleware)
+    )
+);
+
+initializeSagaMiddleware.run(apiSaga);
 
 export default store;
